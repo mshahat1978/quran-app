88788f49ea1c5d6ef53b9ea4ed63e72c884feb5e
Mohammed Ahmed <mohammedahmed1978@gmail.com>
2024-10-09 07:29:26 +0300

    + hadeth Details Screen

--- lib/core/routes_manager.dart
+++ lib/core/routes_manager.dart
@@ -2,4 +2,5 @@ class RouteManager {
   static const String splashRoute = '/';
   static const String homeRoute = '/home';
   static const String quranDetailsRoute = '/QuranDetails';
+  static const String hadethDetailsRoute = '/HadethDetails';
 }
